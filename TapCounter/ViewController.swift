0552870d684afd58f56ee66dc6062209e3652f2d
fweathers <fel.weathers@gmail.com>
2016-04-19 00:08:54 -0400

    First Commit

--- TapCounter/ViewController.swift
+++ TapCounter/ViewController.swift
@@ -9,17 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var counterLabel: UILabel!
+    
+    var counterNumbers : Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        counterLabel.text = "0"
+        
+        
     }
+    
+    @IBAction func tapButtonPressed(sender: UIButton) {
+        
+        self.counterNumbers++
+        self.counterLabel.text = String(self.counterNumbers)
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    @IBAction func resetButtonPressed(sender: UIButton) {
+        
+        self.counterNumbers = 0
+        self.counterLabel.text = String(self.counterNumbers)
+    }
+    
 }
 
